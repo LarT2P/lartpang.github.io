d8ddd51aef4140d28def1aa4d31a7156cc013f41
Lart2P <1304992155@qq.com>
2019-03-14 16:50:00 +0800

    换用了Jane-Hugo

---
+++ update.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+
+# 执行hugo生成页面
+os.system('hugo')
+
+# 复制public到docs文件夹中
+source = './public'
+target = './docs'
+
+
+# 通过校验MD5 判断B内的文件与A 不同
+def get_MD5(file_path):
+    files_md5 = os.popen('md5 %s' % file_path).read().strip()
+    file_md5 = files_md5.replace('MD5 (%s) = ' % file_path, '')
+    return file_md5
+
+
+def main(path, out):
+    for files in os.listdir(path):
+        name = os.path.join(path, files)
+        back_name = os.path.join(out, files)
+        if os.path.isfile(name):
+            if os.path.isfile(back_name):
+                if get_MD5(name) != get_MD5(back_name):
+                    shutil.copy(name, back_name)
+                else:
+                    print(f"{name}文件已是最新")
+            else:
+                shutil.copy(name, back_name)
+        else:
+            if not os.path.isdir(back_name):
+                os.makedirs(back_name)
+            main(name, back_name)
+
+
+if __name__ == '__main__':
+    main(source, target)
+    print("处理完成")
